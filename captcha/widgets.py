3a47f8c0be56b02646b300706fe6c88c42503144
Siddharth Mitra <sidmitra.del@gmail.com>
2011-05-17 15:30:27 +0530

    Added use_ssl boolean kwarg to ReCaptcha widget.

--- captcha/widgets.py
+++ captcha/widgets.py
@@ -7,10 +7,16 @@ class ReCaptcha(forms.widgets.Widget):
     recaptcha_challenge_name = 'recaptcha_challenge_field'
     recaptcha_response_name = 'recaptcha_response_field'
 
+    def __init__(self, *args, **kwargs):
+        self.use_ssl = kwargs.pop('use_ssl', False)
+        super(ReCaptcha, self).__init__(*args, **kwargs)
+    
     def render(self, name, value, attrs=None):
-        use_ssl = False
-        if 'RECAPTCHA_USE_SSL' in settings.__members__:
-            use_ssl = settings.RECAPTCHA_USE_SSL
+        use_ssl = self.use_ssl
+        #use_ssl = False
+        #if 'RECAPTCHA_USE_SSL' in settings.__members__:
+            #use_ssl = settings.RECAPTCHA_USE_SSL
+        
         return mark_safe(u'%s' %
                          captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY,
                                              use_ssl=use_ssl))

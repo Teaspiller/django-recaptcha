f43d0d3652d0fe0ec0e04177f8f273486262d81d
Siddharth Mitra <sidmitra.del@gmail.com>
2011-06-02 01:09:37 +0530

    Added use_ssl kwarg to ReCaptcha widget + fallbacks to settings RECAPTCHA_USE_SSL

--- captcha/widgets.py
+++ captcha/widgets.py
@@ -4,6 +4,12 @@ from django.conf import settings
 from recaptcha.client import captcha
 
 class ReCaptcha(forms.widgets.Widget):
+    """
+    Keyword Arguments:
+    
+        use_ssl: If True, then forces the widget to use SSL. If False, then falls back on 
+                    settings.RECAPTCHA_USE_SSL
+    """
     recaptcha_challenge_name = 'recaptcha_challenge_field'
     recaptcha_response_name = 'recaptcha_response_field'
 
@@ -12,10 +18,9 @@ class ReCaptcha(forms.widgets.Widget):
         super(ReCaptcha, self).__init__(*args, **kwargs)
     
     def render(self, name, value, attrs=None):
-        use_ssl = self.use_ssl
-        #use_ssl = False
-        #if 'RECAPTCHA_USE_SSL' in settings.__members__:
-            #use_ssl = settings.RECAPTCHA_USE_SSL
+        use_ssl = self.use_ssl or False
+        if 'RECAPTCHA_USE_SSL' in settings.__members__:
+            use_ssl = use_ssl or settings.RECAPTCHA_USE_SSL
         
         return mark_safe(u'%s' %
                          captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY,
